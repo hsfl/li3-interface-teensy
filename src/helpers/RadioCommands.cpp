34c4a2d86dcd67f3b26b3ecdd33c2035047c1045
ginoscott <ginoscottdev@gmail.com>
2023-02-09 21:36:22 -1000

    add tcvconfig

--- src/helpers/RadioCommands.cpp
+++ src/helpers/RadioCommands.cpp
@@ -7,6 +7,9 @@ void Lithium3::RadioCommand(Cosmos::Support::PacketComm &packet)
 {
     switch(packet.data[2])
     {
+    case 5:
+        !packet.data[0] ? shared.astrodev_rx.GetTCVConfig(false) : shared.astrodev_tx.GetTCVConfig(false);
+        break;
     case 7:
         !packet.data[0] ? shared.astrodev_rx.GetTelemetry(false) : shared.astrodev_tx.GetTelemetry(false);
         break;
